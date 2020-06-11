d93f650259d8518c4c0e8b9e6ee9cbfc454eb9bb
tiger1016 <applejuicy1016@gmail.com>
2020-06-11 21:17:23 +0200

    video chat with webRTC, TURN, STUN server on express, socket.io

---
+++ src/chat-server.ts
@@ -0,0 +1,76 @@
+import * as express from 'express';
+import { createServer, Server } from 'http';
+import * as socketIo from 'socket.io'; // new
+
+export class ChatServer {
+
+    public static readonly PORT:number = 5000;
+    private app: express.Application;
+    private port: string | number;
+    private server: Server;
+    private io: SocketIO.Server;
+    private socketsArray = [];
+
+    constructor() {
+        this.createApp();
+        this.config();
+        this.createServer();
+        this.sockets();
+        this.listen();
+    }
+
+    private createApp(): void {
+        this.app = express();
+        this.app.use(express.static('public'));
+    }
+
+    private config(): void {
+        this.port = process.env.PORT || ChatServer.PORT;
+    }
+
+    private listen(): void {
+        this.server.listen(this.port, () => {
+            console.log('Running server on port %s', this.port);
+        });
+
+        this.io.on('connection', (socket) => {
+            this.socketsArray.push(socket.id)
+            socket.broadcast.emit('add-users', {
+                users: [socket.id]
+            });
+
+            socket.on('disconnect', () => {
+                this.socketsArray.splice(this.socketsArray.indexOf(socket.id), 1);
+                socket.broadcast.emit('remove-user', socket.id);
+            });
+
+            socket.on('make-offer', (data) => {
+                socket.to(data.to).emit('offer-made', {
+                    offer: data.offer,
+                    socket: socket.id
+                });
+            });
+
+            socket.on('make-answer', (data) => {
+                socket.to(data.to).emit('answer-made', {
+                    socket: socket.id,
+                    answer: data.answer
+                });
+            });
+
+        });
+    }
+
+    private createServer(): void {
+        this.server = createServer(this.app);
+    }
+
+     
+    private sockets(): void {
+        this.io = socketIo(this.server);
+    }
+
+    public getApp(): express.Application {
+        return this.app;
+    }
+}
